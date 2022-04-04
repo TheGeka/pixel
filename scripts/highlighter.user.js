fb8a34f4bfb4c6f0836227e56f80ea83e5f9aa4d
raffimolero <49224759+raffimolero@users.noreply.github.com>
2022-04-04 23:28:41 +0800

    fixed phantom pixels showing up
    you know the taskbar on the philippine flag?
    i took that patriotically.

--- scripts/highlighter.user.js
+++ scripts/highlighter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Highlight Tool
 // @namespace    http://tampermonkey.net/
-// @version      0.8
+// @version      0.8.1
 // @description  try to take over the canvas!
 // @author       oralekin, LittleEndu, ekgame, Wieku, DeadRote, Thank you Osu Server for the Highlight feature - Senpoii
 // @match        https://hot-potato.reddit.com/embed*
@@ -13,26 +13,17 @@ if (window.top !== window.self) {
         // Load the image
         const image = document.createElement("img");
         const undotted = "https://dev.bloodmoon-network.de/place/HighlightShadowmap.png";
-        image.src = "https://dev.bloodmoon-network.de/place/output.png";
-
-        image.onload = () => {
-             image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
-        };
 
         // Add the image as overlay
         const camera = document.querySelector("mona-lisa-embed").shadowRoot.querySelector("mona-lisa-camera");
         const layout = document.querySelector("mona-lisa-embed").shadowRoot;
         const canvas = camera.querySelector("mona-lisa-canvas");
-        canvas.shadowRoot.querySelector('.container').appendChild(image);
       
         // Add a style to put a hole in the pixel preview (to see the current or desired color)
         const waitForPreview = setInterval(() => {
             const preview = camera.querySelector("mona-lisa-pixel-preview");
             if (preview) {
               clearInterval(waitForPreview);
-              const style = document.createElement('style')
-              style.innerHTML = '.pixel { clip-path: polygon(-20% -20%, -20% 120%, 37% 120%, 37% 37%, 62% 37%, 62% 62%, 37% 62%, 37% 120%, 120% 120%, 120% -20%); }'
-              preview.shadowRoot.appendChild(style);
               loadRegions()
               setTimeout(()=>{
                   loadRegions();
@@ -44,6 +35,7 @@ if (window.top !== window.self) {
               },1000)
             }
         }, 100);
+      
         //Insert element after another element
         function insertAfter(newNode, referenceNode) {
             referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
